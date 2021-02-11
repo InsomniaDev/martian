b9c17488a402e73a273f074481fe666b2efe4a12
InsomniaDev <adam9190@gmail.com>
2021-02-10 21:16:36 -0700

    Clean up the data access model

--- src/main/scala/modules/databaseQueries/config.scala
+++ src/main/scala/modules/databaseQueries/config.scala
@@ -12,36 +12,30 @@ class ConfigData(ctx: PostgresJdbcContext[SnakeCase.type]) {
   import ctx._
 
   def getConfigByKey(configKey: Option[String]): List[Config] = {
-    val q = quote {
+    run {
       query[Config]
         .filter(_.key == lift(configKey))
     }
-    val resp = ctx.run(q)
-    resp
   }
 
   def getConfig(): List[Config] = {
-    val q = quote {
+    run {
       query[Config]
     }
-    val resp = ctx.run(q)
-    resp
   }
 
   def insertConfigItem(config: Config) = {
-    val q = quote {
+    run {
       query[Config]
         .insert(
           _.key -> lift(config.key),
           _.value -> lift(config.value)
         )
     }
-    val resp = ctx.run(q)
-    resp
   }
 
   def updateConfig(config: Config) = {
-    val q = quote {
+    run {
       query[Config]
         .filter(_.id == lift(config.id))
         .update(
@@ -49,17 +43,13 @@ class ConfigData(ctx: PostgresJdbcContext[SnakeCase.type]) {
           _.value -> lift(config.value)
         )
     }
-    val resp = ctx.run(q)
-    resp
   }
 
   def deleteConfig(config: Config) = {
-    val q = quote {
+    run {
       query[Config]
         .filter(_.id == lift(config.id))
         .delete
     }
-    val resp = ctx.run(q)
-    resp
   }
 }
--- src/main/scala/modules/databaseQueries/fact.scala
+++ src/main/scala/modules/databaseQueries/fact.scala
@@ -1,4 +1,5 @@
 package modules.databaseQueries
+
 import io.getquill._
 
 case class Fact(
@@ -12,43 +13,35 @@ class FactData(ctx: PostgresJdbcContext[SnakeCase.type]) {
   import ctx._
 
   def getFact(factId: Int): List[Fact] = {
-    val q = quote {
+    run {
       query[Fact]
         .filter(a => a.id == lift(factId))
     }
-    val resp = ctx.run(q)
-    resp
   }
 
   def getFacts(factId: Int): List[Fact] = {
-    val q = quote {
+    run {
       query[Fact]
     }
-    val resp = ctx.run(q)
-    resp
   }
 
   def getRelatedFactIds(factName: List[String]): List[Option[String]] = {
-    val q = quote {
+    run {
       query[Fact]
         .filter(a => liftQuery(factName).contains(a.related_facts))
         .map(a => a.related_fact_ids)
     }
-    val resp = ctx.run(q)
-    resp
   }
 
   def insertFact(fact: Fact) = {
-    val q = quote {
+    run {
       query[Fact]
         .insert(lift(fact))
     }
-    val resp = ctx.run(q)
-    resp
   }
 
   def updateRelatedFacts(fact: Fact) = {
-    val q = quote {
+    run {
       query[Fact]
         .filter(_.id == lift(fact.id))
         .update(
@@ -56,27 +49,21 @@ class FactData(ctx: PostgresJdbcContext[SnakeCase.type]) {
           _.related_fact_ids -> lift(fact.related_fact_ids)
         )
     }
-    val resp = ctx.run(q)
-    resp
   }
 
   def updateFactData(fact: Fact) = {
-    val q = quote {
+    run {
       query[Fact]
         .filter(_.id == lift(fact.id))
         .update(_.fact_data -> lift(fact.fact_data))
     }
-    val resp = ctx.run(q)
-    resp
   }
 
   def deleteFactData(fact: Fact) = {
-    val q = quote {
+    run {
       query[Fact]
         .filter(_.id == lift(fact.id))
         .delete
     }
-    val resp = ctx.run(q)
-    resp
   }
 }
