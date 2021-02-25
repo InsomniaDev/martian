1f5adfe6554d44ce8ea1330c0b670642f1d349d7
InsomniaDev <adam9190@gmail.com>
2021-02-24 20:42:06 -0700

    Retrieving facts by a provided string should now be done

--- src/main/scala/modules/databaseQueries/fact.scala
+++ src/main/scala/modules/databaseQueries/fact.scala
@@ -7,7 +7,8 @@ case class Fact(
     name: String,
     related_fact_ids: String,
     related_facts: String,
-    fact_data: Option[String]
+    fact_data: Option[String],
+    importance: Int
 )
 
 case class FactsToWords(
@@ -24,6 +25,61 @@ case class Word(
 
 class FactData(ctx: PostgresJdbcContext[SnakeCase.type]) {
   import ctx._
+
+  /** getFactsByUsedWords
+    * 
+    * SELECT x1.id, x1.fact_id, x1.word_id, x1.importance, a.id, a.word 
+    * FROM facts_to_words x1 
+    *   INNER JOIN (
+    *     SELECT a.id, a.word 
+    *     FROM word a 
+    *     WHERE a.word IN (?)
+    *   ) AS a 
+    *   ON x1.word_id = a.id 
+    *   WHERE a.word = ?
+    *
+    * @param words
+    * @return
+    */
+  def getFactsByUsedWords(words: List[String]): List[(FactsToWords, Word)] = {
+    run {
+      query[FactsToWords]
+        .join(query[Word].filter(a => liftQuery(words).contains(a.word)))
+        .on(_.word_id == _.id)
+        .filter({ case (ftw, w) => w.word == lift(words(1)) })
+    }
+  }
+
+  /** incrementFactToWordImportance
+    * 
+    * UPDATE facts_to_words 
+    * SET importance = (importance + 1) 
+    * WHERE id = ?
+    *
+    * @param id
+    */
+  def incrementFactToWordImportance(id: Int) = {
+    run {
+      query[FactsToWords]
+      .filter(_.id == lift(id))
+      .update(p =>
+        p.importance -> (p.importance + 1)
+      )
+    }
+  }
+
+  /** getFactsByIds
+    * 
+    * SELECT a.id, a.name, a.related_fact_ids, a.related_facts, a.fact_data 
+    * FROM fact a 
+    * WHERE a.id IN (?)
+    *
+    * @param factIds
+    * @return
+    */
+  def getFactsByIds(factIds: List[Int]): List[Fact] = {
+    run(query[Fact].filter(a => liftQuery(factIds).contains(a.id)))
+  }
 
   def getFact(factId: Int): List[Fact] = {
     run {
@@ -53,16 +109,6 @@ class FactData(ctx: PostgresJdbcContext[SnakeCase.type]) {
   // checkFactNames gets the fact back by the provided name
   def checkFactNames(factName: List[String]): List[Fact] = {
     run(query[Fact].filter(a => liftQuery(factName).contains(a.name)))
-  }
-
-  // getFactsByUsedWords returns the list of relationships and the words used
-  def getFactsByUsedWords(words: List[String]): List[(FactsToWords, Word)] = {
-    run {
-      query[FactsToWords]
-        .join(query[Word].filter(a => liftQuery(words).contains(a.word)))
-        .on(_.word_id == _.id)
-        .filter({ case (ftw, w) => w.word == lift(words(1)) })
-    }
   }
 
   def getRelatedFactIds(factName: String): List[String] = {
--- src/main/scala/modules/knowledge/parseFacts.scala
+++ src/main/scala/modules/knowledge/parseFacts.scala
@@ -6,58 +6,50 @@ import io.getquill._
 // TODO: Could we possibly export all of this knowledge data into markdown files and then display them through something like Hugo?
 // https://gohugo.io/documentation/
 
+// TODO: Provide option for the editing user to relate facts
+// TODO: Facts should have an importance based on the number of related facts
+
 class FactParser(ctx: PostgresJdbcContext[SnakeCase.type])
     extends FactData(ctx) {
 
-  private def getNumberOfMatches(value: List[String]): Int = {
-    println("hello")
+  /** getMatchesForAllWords
+    *
+    * Get all of the matches for the words that are provided and then order them by count and prevalence
+    *
+    * Return the top three results and increment importance
+    *
+    * @param words is a list of words that aren't commonly used and are currently being searched
+    */
+  private def getMatchesForAllWords(words: List[String]): Seq[FactsToWords] = {
 
-    1
-  }
-
-/** getMatchesForAllWords
-  * 
-  * Get all of the matches for the words that are provided and then order them by count and prevalence
-  * 
-  * Return the top three results and increment importance
-  *
-  * @param value
-  */
-  private def getMatchesForAllWords(value: String) = {
     // TODO: Need to create a unit test for this functionality
-    val words = value.split(" ").toList
     val resp = getFactsByUsedWords(words)
 
     // Need to group by the fact_id
     // **IMPORTANT** This counts on a unique relationship between fact_id and word_id
-    // TODO: Add unique relationship between fact_id and word_id in the database
     val distinctFacts = resp.groupBy(_._1.fact_id)
 
     // Sort by the highest amount of matches
-    val highestFoundWords = distinctFacts.toSeq.sortWith(_._1 > _._1)
+    val highestFoundWords =
+      distinctFacts.toSeq.sortWith(_._1 > _._1).take(3).flatMap(_._2)
+
+    // Return the matched list sorted by the importance level
+    highestFoundWords.map(_._1).sortWith(_.importance > _.importance)
   }
 
-  /** checkForFact
+  /** getFactsByFoundIds
     *
-    * This method retrieves all of the fact data for the provided fact names along with their related facts
+    * This will get all of the facts by the provided ids
+    * will return a sorted by importance sequence    *
     *
-    * @param value is the string passed in to look at
-    * @return a list of fact data
+    * @param factIds
+    * @return
     */
-  private def checkForFact(value: List[String]): List[String] = {
+  private def getFactsByFoundIds(factIds: Seq[Int]): Seq[Fact] = {
 
-    // Get all of the facts that match the provided list of values
-    val resp = checkFactNames(value)
+    val foundFacts = getFactsByIds(factIds.toList)
 
-    // TODO: Possibly search for the ones that have the highest count of words...
-
-    // Get all of the fact data from the related facts
-    val relatedFacts = getRelatedFactsByIds(
-      resp.flatMap(a => a.related_fact_ids.split(";").map(_.toInt))
-    ).flatMap(_.fact_data)
-
-    // Take all of the returned facts and put into a single array of facts to return
-    resp.flatMap(_.fact_data) ++ relatedFacts
+    foundFacts.sortWith(_.importance > _.importance)
   }
 
   /** decipherKnowledgeString
@@ -67,7 +59,7 @@ class FactParser(ctx: PostgresJdbcContext[SnakeCase.type])
     * @param value
     * @return the relevant fact set
     */
-  def decipherKnowledgeString(value: String): List[String] = {
+  def decipherKnowledgeString(value: String): Option[List[Fact]] = {
 
     // Get all of the commonWords from the database
     val conf = new ConfigData(ctx)
@@ -79,6 +71,39 @@ class FactParser(ctx: PostgresJdbcContext[SnakeCase.type])
     // Get all of the values that aren't in the common words list
     val parsedValues = value.split(" ").view.filter(!conf.contains(_)).toList
 
-    checkForFact(parsedValues)
+    // Get all of the top matches by the count of words matched ordered by importance level
+    val topImportantMatches = getMatchesForAllWords(parsedValues)
+
+    // If there are relevant matches for the words provided
+    topImportantMatches match {
+      case tim if (tim.length > 0) => {
+
+        // Increment the importance for the returned matches
+        topImportantMatches.foreach(a => incrementFactToWordImportance(a.id))
+
+        // Get the facts for these matches sorted by the fact importance
+        Some(getFactsByFoundIds(topImportantMatches.map(_.fact_id)).toList)
+      }
+      case _ => None
+    }
   }
 }
+
+
+
+// This function might be deprecated
+  // private def checkForFact(value: List[String]): List[String] = {
+
+  //   // Get all of the facts that match the provided list of values
+  //   val resp = checkFactNames(value)
+
+  //   // TODO: Possibly search for the ones that have the highest count of words...
+
+  //   // Get all of the fact data from the related facts
+  //   val relatedFacts = getRelatedFactsByIds(
+  //     resp.flatMap(a => a.related_fact_ids.split(";").map(_.toInt))
+  //   ).flatMap(_.fact_data)
+
+  //   // Take all of the returned facts and put into a single array of facts to return
+  //   resp.flatMap(_.fact_data) ++ relatedFacts
+  // }
