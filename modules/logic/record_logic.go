ac94983e80da2ab92d401f1ebe4e0eafc66b6f39
InsomniaDev <adam9190@gmail.com>
2021-05-02 22:11:34 -0600

    additional todo

--- modules/logic/record_logic.go
+++ modules/logic/record_logic.go
@@ -3,6 +3,7 @@ package logic
 import "fmt"
 
 func SortAndRetrieveRecordUuids(records [][]string, numOfRecordsToReturn int) []string {
+	// TODO: Need to add in this functionality
 	fmt.Println("not currently implemented")
 	return records[0]
 }
