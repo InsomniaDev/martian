710ff9feb77ce49a5b80118e6c4c35548439b922
InsomniaDev <adam9190@gmail.com>
2021-05-19 22:44:17 -0600

    fix bug and add todos

--- modules/logic/parse_insert.go
+++ modules/logic/parse_insert.go
@@ -16,12 +16,16 @@ func ParseInsert(conn *cassandra.Session, accountUuid gocql.UUID) {
 
 func UpsertRecord(conn *cassandra.Session, record cassandra.Record) bool {
 
+	inserted := conn.UpsertRecord(record)
+
 	// Create the word association with the record
 	var wordRecords []cassandra.WordsToRecord
 	for _, word := range record.Words {
 		wordRecords = append(wordRecords, cassandra.WordsToRecord{Word: word, AccountUuid: record.AccountUuid, RecordUuid: record.RecordUuid.String()})
 	}
 	conn.UpsertMultipleWordsToRecord(wordRecords)
-	
-	return conn.UpsertRecord(record)
+
+	// TODO: need to upsert the entities to the record here
+
+	return inserted
 }
