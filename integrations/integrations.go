db39dd979073547f2978d3ba3b177da876762a67
InsomniaDev <adam9190@gmail.com>
2021-07-10 07:24:20 -0600

    Reenable lutron

--- integrations/integrations.go
+++ integrations/integrations.go
@@ -21,7 +21,7 @@ type Integrations struct {
 
 func (i *Integrations) Init() {
 	// TODO: This needs to load up each based on if it is available, there is no point in loading up all of them
-	// i.LutronData = lutron.Init()
+	i.LutronData = lutron.Init()
 	i.HarmonyData.Init()
 	// i.KasaData.Init()
 	i.Menu = config.LoadMenu()
