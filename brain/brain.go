d950bf80f393db641dfcce2807f8c70dbe980d9a
InsomniaDev <adam9190@gmail.com>
2022-01-28 14:47:40 -0700

    adds debug lines

--- brain/brain.go
+++ brain/brain.go
@@ -67,7 +67,7 @@ func init() {
 func (b *Brain) brainWave(id, status string) {
 	// TODO: Add more logic here, just append for now
 	b.memoryEvent = append(b.memoryEvent, event{eventId: id, eventStatus: status, eventTime: time.Now(), eventTimeExpiration: time.Now().Add(timeDifference)})
-	log.Debug("stored event", id, status)
+	log.Debug("stored event:", id, status)
 }
 
 // shortTerm checks to see if the current timestamp is greater than the eventTimeExpiration
@@ -100,12 +100,14 @@ func (b *Brain) shortTerm() {
 				var hourEventsRemembered []longTermStore
 				if err := json.Unmarshal(resp, &hourEventsRemembered); err != nil {
 					log.WithFields(log.Fields{
-						"resp":string(resp),
+						"resp": string(resp),
 					}).Error("Error getting short term source from the brain ", err)
 				}
 
 				// Store away all of the events for the hour here
 				hourEventsRemembered = append(hourEventsRemembered, remember)
+				whatis, _ := json.Marshal(hourEventsRemembered)
+				log.Println(hourEventsRemembered, string(whatis))
 				database.MartianData.InsertMemory(strconv.Itoa(hourOfOccurrence), hourEventsRemembered)
 			}
 		}
@@ -131,6 +133,8 @@ func (b *Brain) processDayMemories() {
 
 	// go through each memory hour from the last 24 hours
 	for key, memory := range memories {
+		log.Println(key, string(memory))
+
 		// convert data into struct
 		var hourEventsRemembered []longTermStore
 		if err := json.Unmarshal(memory, &hourEventsRemembered); err != nil {
